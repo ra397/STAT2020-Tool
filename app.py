b120ac714e49111332ac9f8d1dd1422346d4cdfd
Rabi Alaya <rabialaya@gmail.com>
2022-05-05 16:22:20 -0500

    Create visualization app

---
+++ app.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+# The main window
+window = Tk()
+
+# setting the title and
+window.title('Visualization App')
+
+# setting the dimensions of
+# the main window
+window.geometry("500x500")
+
+
+# run the gui
+window.mainloop()
